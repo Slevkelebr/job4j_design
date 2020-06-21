9907d04d8cb8d906fb25838c2bf65613e55a4171
Slevkelebr <slevkelebr@yandex.ru>
2020-06-21 17:20:52 +0500

    1. Читаем файл конфигурации [#245941]

---
+++ chapter_001/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,49 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines()
+                    .filter(i -> !(i.isEmpty() || i.startsWith("#")))
+                    .forEach(i -> {
+                        String[] split = i.split("=");
+                        this.values.put(split[0], split[1]);
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return this.values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config(Objects.requireNonNull(Config.class.getClassLoader().getResource("app.properties")).getFile()));
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = Config.class.getClassLoader().getResource("app.properties").getFile();
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Petr Arsentev")
+        );
+    }
+}
