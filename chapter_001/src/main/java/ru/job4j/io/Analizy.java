615c043a195f61650003a825b66e495c4970404f
Slevkelebr <slevkelebr@yandex.ru>
2020-08-27 19:10:53 +0500

    2. Анализ доступности сервера. [#245942] (избавился от удержания потока вывода)

--- chapter_001/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_001/src/main/java/ru/job4j/io/Analizy.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Класс читает данные из файла и преобразовывает их в другой файл.
@@ -11,20 +13,33 @@ import java.io.*;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        try (BufferedReader in = new BufferedReader(new FileReader(source));
-             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             String serverNotAvailable = null;
+            List<String> list = new ArrayList<>();
             while (in.ready()) {
                 String string = in.readLine();
                 if (serverNotAvailable == null && (string.startsWith("400") || string.startsWith("500"))) {
-                    out.print(string.substring(4) + ";");
+                    list.add(string.substring(4) + ";");
                     serverNotAvailable = string;
                 } else if (serverNotAvailable != null && (!string.startsWith("400") && (!string.startsWith("500")))) {
-                    out.println(string.substring(4));
+                    list.add(string.substring(4));
                     serverNotAvailable = null;
                 }
             }
-        } catch (Exception e) {
+            PrintWriter out = new PrintWriter(new FileOutputStream(target));
+            int strIndex = 0;
+            for (var str :
+                 list) {
+                if (strIndex == 0) {
+                    out.print(str);
+                    strIndex++;
+                } else if (strIndex == 1) {
+                    out.println(str);
+                    strIndex = 0;
+                }
+            }
+            out.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
