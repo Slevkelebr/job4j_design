282b349e77f6f3d3e1ede9ecf4ef7e0b791f7375
Slevkelebr <slevkelebr@yandex.ru>
2020-09-04 09:22:36 +0500

    0. Видео [#245951] (Общение Клиент-Сервер) (Рефакторинг)

--- chapter_001/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_001/src/main/java/ru/job4j/io/EchoServer.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            while (true) {
+            while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
@@ -18,8 +18,7 @@ public class EchoServer {
                         if (str.contains("=")) {
                             String[] text = str.split("=");
                             if (text[1].contains("Exit")) {
-                                socket.close();
-                                break;
+                                answer = "Server closed";
                             } else if (text[1].contains("Hello")) {
                                 answer = "Hello";
                             } else {
@@ -29,12 +28,12 @@ public class EchoServer {
                         }
                         System.out.println(str);
                     }
-                    if (socket.isClosed()) {
-                        server.close();
-                        break;
-                    }
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     out.write(answer.getBytes());
+                    if (answer.contains("closed")) {
+                        socket.close();
+                        server.close();
+                    }
                 }
             }
         }
