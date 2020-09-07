f45ead7ecad463456c73eade1db3ab65822de1ea
Slevkelebr <slevkelebr@yandex.ru>
2020-09-07 18:52:19 +0500

    5.1.4. Создать convert(Iterator<Iterator>) [#245912] (Рефакторинг)

--- chapter_001/src/main/java/ru/job4j/it/FlatMap.java
+++ chapter_001/src/main/java/ru/job4j/it/FlatMap.java
@@ -1,5 +1,6 @@
 package ru.job4j.it;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -13,11 +14,10 @@ import java.util.NoSuchElementException;
 
 public class FlatMap<T> implements Iterator<T> {
     private final Iterator<Iterator<T>> data;
-    private Iterator<T> cursor;
+    private Iterator<T> cursor = Collections.emptyIterator();
 
     public FlatMap(Iterator<Iterator<T>> data) {
         this.data = data;
-        this.cursor = data.next();
     }
 
     /**
