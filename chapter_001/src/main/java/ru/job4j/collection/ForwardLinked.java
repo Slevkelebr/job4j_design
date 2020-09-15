8834221cfe3d14b2c6b609219878449a245dcc97
Slevkelebr <slevkelebr@yandex.ru>
2020-09-15 14:20:51 +0500

    5.3.0 Создать метод delete для односвязного списка [#245903]

---
+++ chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,68 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Класс описывает контейнер для ссылочных типов данных.
+ * @author Frolov Sergey (Slevkelebr@yandex.ru)
+ * @version 0.1
+ * @since 15.09.2020
+ */
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public void deleteFirst() {
+        if (head != null) {
+            head = head.next;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
