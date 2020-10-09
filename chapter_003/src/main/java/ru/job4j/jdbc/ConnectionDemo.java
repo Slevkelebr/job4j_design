381c66801e09659149fbb1fb971ba1f9d0e5c371
Slevkelebr <slevkelebr@yandex.ru>
2020-10-09 14:59:20 +0500

    0. Подключение JDBC [#245879] (Использовал Properties)

--- chapter_003/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ chapter_003/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,44 +1,26 @@
 package ru.job4j.jdbc;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.Properties;
+
 
 public class ConnectionDemo {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Class.forName("org.postgresql.Driver");
-        List<String> values = loadDB();
-        String url = values.get(0);
-        String login = values.get(1);
-        String password = values.get(2);
+        Properties properties = new Properties();
+        FileInputStream in = new FileInputStream("c:\\projects\\job4j_design\\chapter_003\\src\\main\\resources\\app.properties");
+        properties.load(in);
+        String url = properties.getProperty("jdbc.url");
+        String login = properties.getProperty("jdbc.username");
+        String password = properties.getProperty("jdbc.password");
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
             System.out.println(metaData.getURL());
         }
-    }
-
-    public static List<String> loadDB() {
-       List<String> values = new ArrayList<>();
-        try (BufferedReader read = new BufferedReader(new FileReader(
-                Objects.requireNonNull(ConnectionDemo.class.getClassLoader().getResource("app.properties")).getFile()))) {
-            read.lines()
-                    .filter(i -> !(i.isEmpty() || i.startsWith("#")))
-                    .forEach(i -> {
-                        if (i.contains("=")) {
-                            String[] split = i.split("=");
-                            values.add(split[1]);
-
-                        }
-                    });
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        }
-        return values;
     }
 }
