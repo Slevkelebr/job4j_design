095cc0126b53461066dcb68ed80e1b212a2d6fb8
Slevkelebr <slevkelebr@yandex.ru>
2020-10-10 20:57:47 +0500

    0.1. Statement [#385466]

---
+++ chapter_003/src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,112 @@
+package ru.job4j.jdbc;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private final Properties properties;
+
+    public TableEditor(Properties properties) throws ClassNotFoundException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        try (FileInputStream in = new FileInputStream("c:\\projects\\job4j_design\\chapter_003\\src\\main\\resources\\app.properties")) {
+            properties.load(in);
+            String url = properties.getProperty("jdbc.url");
+            String login = properties.getProperty("jdbc.username");
+            String password = properties.getProperty("jdbc.password");
+            connection = DriverManager.getConnection(url, login, password);
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createTable(String tableName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "create table if not exists %s();",
+                    tableName
+            );
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dropTable(String table) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "drop table %s;",
+                    table
+            );
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "ALTER TABLE %s ADD COLUMN IF NOT EXISTS %s %s;",
+                    tableName, columnName, type
+            );
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "ALTER TABLE %s DROP COLUMN %s;",
+                    tableName, columnName
+            );
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = String.format(
+                    "ALTER TABLE %s RENAME COLUMN %s TO %s;",
+                    tableName, columnName, newColumnName
+            );
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getScheme(String tableName) throws SQLException {
+        StringBuilder scheme = new StringBuilder();
+        DatabaseMetaData metaData = connection.getMetaData();
+        try (ResultSet columns = metaData.getColumns(null, null, tableName, null)) {
+            scheme.append(String.format("%-15s %-15s%n", "column", "type"));
+            while (columns.next()) {
+                scheme.append(String.format("%-15s %-15s%n",
+                        columns.getString("COLUMN_NAME"),
+                        columns.getString("TYPE_NAME")));
+            }
+        }
+        return scheme.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
