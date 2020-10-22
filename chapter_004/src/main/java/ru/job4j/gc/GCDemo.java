9baeba7ca80eb20970617253f933faa5260ffad6
Slevkelebr <Slevkelebr@yandex.ru>
2020-10-22 20:16:47 +0500

    Демонстрация работы GC

---
+++ chapter_004/src/main/java/ru/job4j/gc/GCDemo.java
@@ -0,0 +1,31 @@
+package ru.job4j.gc;
+
+/**
+ * Демонстрация работы GC с ключом ограничения размера heap -Xmx4m -Xms4m.
+ */
+public class GCDemo {
+
+    private static final long KB = 1000;
+    private static final long MB = KB * KB;
+    private static final Runtime ENVIRONMENT = Runtime.getRuntime();
+
+    public static void info() {
+        final long freeMemory = ENVIRONMENT.freeMemory();
+        final long totalMemory = ENVIRONMENT.totalMemory();
+        final long maxMemory = ENVIRONMENT.maxMemory();
+        System.out.println("=== Environment state ===");
+        System.out.printf("Free: %d%n", freeMemory / MB);
+        System.out.printf("Total: %d%n", totalMemory / MB);
+        System.out.printf("Max: %d%n", maxMemory / MB);
+    }
+
+    public static void main(String[] args) {
+        info();
+        for (int i = 0; i < 10; i++) {
+            new Person(i, "N" + i);
+        }
+        System.gc();
+        info();
+    }
+
+}
