3064314e83992a223248184b205abe7f57d55053
Slevkelebr <Slevkelebr@yandex.ru>
2020-10-27 11:30:29 +0500

    Анализировал работу разных GC

--- chapter_004/src/main/java/ru/job4j/gc/TestGC.java
+++ chapter_004/src/main/java/ru/job4j/gc/TestGC.java
@@ -1,12 +1,10 @@
 package ru.job4j.gc;
 
 import javax.management.*;
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
-import java.util.Arrays;
 
 public class TestGC {
     private static final Runtime ENVIRONMENT = Runtime.getRuntime();
+
 
     public static void info() {
         final long freeMemory = ENVIRONMENT.freeMemory();
@@ -18,25 +16,11 @@ public class TestGC {
         System.out.printf("Max: %d%n", maxMemory);
     }
 
-
-    private static void defineGC () throws MalformedObjectNameException, MBeanException, InstanceNotFoundException, ReflectionException {
-        Object flags = ManagementFactory.getPlatformMBeanServer().invoke(
-                ObjectName.getInstance("com.sun.management:type=DiagnosticCommand"),
-                "vmFlags", new Object[] { null }, new String[] { "[Ljava.lang.String;" });
-        for(String f: ((String)flags).split("\\s+"))
-            if(f.contains("GC")) System.out.println(f);
-        for(GarbageCollectorMXBean gc: ManagementFactory.getGarbageCollectorMXBeans())
-            System.out.printf("%-20s%s%n", gc.getName(), Arrays.toString(gc.getMemoryPoolNames()));
-    }
-
-    public static void main(String[] args) throws MalformedObjectNameException, MBeanException, InstanceNotFoundException, ReflectionException {
-        defineGC();
-        info();
-        for (int i = 0; i < 4500; i++) {
+    public static void main(String[] args) {
+        for (int i = 0; i < 25000; i++) {
             new User("N" + i, i);
         }
-        //System.gc();
-        info();
     }
+
 
 }
