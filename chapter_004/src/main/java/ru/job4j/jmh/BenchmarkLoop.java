3c629e28067dfac84a1f584aa67c65f82b53eca2
Slevkelebr <Slevkelebr@yandex.ru>
2020-10-28 19:08:06 +0500

    Test benchmark JMH

---
+++ chapter_004/src/main/java/ru/job4j/jmh/BenchmarkLoop.java
@@ -0,0 +1,85 @@
+package ru.job4j.jmh;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime) //Режим работы
+@OutputTimeUnit(TimeUnit.MILLISECONDS) //Вывод результата в ед.измерениях
+@State(Scope.Benchmark) //Данные хранятся в одном потоке
+@Warmup(iterations = 5) // этап прогрева VM
+@Measurement(iterations = 5) // этап измерений
+@Fork(value = 2) // кол-во повторов этапов
+
+public class BenchmarkLoop {
+
+    @Param({"10000000"})
+    private int n;
+
+    private List<String> dataForTesting;
+
+    public static void main(String[]args) throws RunnerException {
+
+        Options opt = new OptionsBuilder()
+                .include(BenchmarkLoop.class.getSimpleName())
+                .forks(2)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Setup
+    public void setup() {
+        dataForTesting = createData();
+    }
+
+    @Benchmark
+    public void loopFor(Blackhole bh) {
+        for (int i = 0; i < dataForTesting.size(); i++) {
+            String s = dataForTesting.get(i); //take out n consume, fair with foreach
+            bh.consume(s);
+        }
+    }
+
+    @Benchmark
+    public void loopWhile(Blackhole bh) {
+        int i = 0;
+        while (i < dataForTesting.size()) {
+            String s = dataForTesting.get(i);
+            bh.consume(s);
+            i++;
+        }
+    }
+
+    @Benchmark
+    public void loopForEach(Blackhole bh) {
+        for (String s : dataForTesting) {
+            bh.consume(s);
+        }
+    }
+
+    @Benchmark
+    public void loopIterator(Blackhole bh) {
+        Iterator<String> iterator = dataForTesting.iterator();
+        while (iterator.hasNext()) {
+            String s = iterator.next();
+            bh.consume(s);
+        }
+    }
+
+    private List<String> createData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            data.add("Number : " + i);
+        }
+        return data;
+    }
+}
